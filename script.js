bce590093802f3d24d2561a42906625786488665
Jurgen Galicia <jurgen.galicia@gmail.com>
2022-08-13 21:15:29 -0700

    created checkwinner and playRPS functions

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let choicesList = ["Rock", "Paper", "Scissors"];
+let choicesList = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
-    return choicesList[Math.floor((Math.random() * 3) + 1) - 1];
+    return Math.floor((Math.random() * 3) + 1) - 1;
+}
+
+function checkWinner(choice1, choice2){
+    if((choice1 === "rock" && choice2 == "scissors") ||
+        (choice1 === "paper" && choice2 == "rock") ||
+        (choice1 === "scissors" && choice2 == "paper"))
+        return true;
+    return false;
+}
+
+function playRPS(playerChoice, computerChoice){
+    let lowerPlayer = playerChoice.toLowerCase();
+    let lowerComputer = computerChoice.toLowerCase();
+    let capitalPlayer = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+    let capitalComputer = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    if(lowerComputer === lowerPlayer)
+        return "It's a draw!"
+    else if(checkWinner(lowerPlayer,lowerComputer))
+        return `You win! ${capitalPlayer} beats ${capitalComputer}`
+    else
+        return `You lose... ${capitalComputer} beats ${capitalPlayer}`
 }
 
 console.log(getComputerChoice())
