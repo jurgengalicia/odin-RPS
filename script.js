295c78c774d9b27750219cd5d5bdef6b08e706b1
JurgenGalicia <jurgen.galicia@gmail.com>
2026-05-24 18:27:17 -0700

    cleaned up global variables, playGame viable

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let handOptions = ["rock", "paper", "scissors"]
     let pickHand = Math.floor(Math.random() * 3);
     return handOptions[pickHand];
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Please enter your hand choice: rock, paper or scissors", "Rock").toLowerCase();
-    return humanChoice;
-}
+// prompt user for a choice
+let getHumanChoice = () => prompt("Please enter your hand choice: rock, paper or scissors", "Rock").toLowerCase();
 
-function pluralize(word, num){
-    return num == 1 ? word : `${word}s`
-}
+//determine if a given word should be plural based on num
+let pluralize = (word, num) => num == 1 ? word : `${word}s`;
 
-function playRound(compChoice,humanChoice){
+function playRound(compChoice,humanChoice,tally){
     let scoreingSheet = {"rock":"scissors", "scissors":"paper", "paper":"rock"}
     console.log(`Your choice: ${humanChoice}`)
     console.log(`Computer's choice: ${compChoice}`)
     if(scoreingSheet[compChoice] === humanChoice){
-        computerScore += 1;
+        tally.computerScore += 1;
         console.log(`You lose this round, ${compChoice} beats ${humanChoice}`)
     } else if(scoreingSheet[humanChoice] === compChoice){
-        humanScore += 1;
+        tally.humanScore += 1; 
         console.log(`You win this round! ${humanChoice} beats ${compChoice}`)
     } else {
         console.log("its a tie");
     }
-    console.log(`Current score, you: ${humanScore}, computer: ${computerScore}`)
+    console.log(`Current score, you: ${tally.humanScore}, computer: ${tally.computerScore}`)
 }
 
 function playGame(){
+    let tally = {humanScore: 0, computerScore: 0};
+
     for(let i = 0; i < 5; i++){
-        playRound(getComputerChoice(), getHumanChoice())
+        playRound(getComputerChoice(), getHumanChoice(),tally)
     }
+    let humanScore = tally.humanScore;
+    let computerScore = tally.computerScore;
     if(humanScore > computerScore){
         console.log(`You win the game! You scored ${humanScore} ${pluralize('point',humanScore)} when the computer got ${computerScore} ${pluralize('point',computerScore)}.`)
     } else if (humanScore < computerScore) {
